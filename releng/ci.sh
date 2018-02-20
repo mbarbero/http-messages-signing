63ec722a9b61753a6cb011bc4d3c3c096d2940b7
Mikael Barbero <mikael.barbero@eclipse-foundation.org>
2018-02-20 11:29:39 +0100

    Update build env to mvn 3.5.2 after issue with spotbugs
    See https://github.com/spotbugs/spotbugs-maven-plugin/issues/21

--- releng/ci.sh
+++ releng/ci.sh
@@ -12,8 +12,8 @@ mvn -e -C -U -V -B clean verify -Pcompile -f "${SCRIPT_PATH}/../pom.xml"
 
 export GPG_HOME="${SCRIPT_PATH}/gnupg"
 
-openssl aes-256-cbc -pass pass:${ENCRYPTION_PASSPHRASE} \
+openssl aes-256-cbc -pass pass:${ENCRYPTION_PASSPHRASE} -d -a \
 	-in ${GPG_HOME}/private-keys-v1.d/F87B14B93BCEF6259480B139A63A13E97B306101.key.enc \
-	-out ${GPG_HOME}/private-keys-v1.d/F87B14B93BCEF6259480B139A63A13E97B306101.key -d
+	-out ${GPG_HOME}/private-keys-v1.d/F87B14B93BCEF6259480B139A63A13E97B306101.key
 
 mvn -e -C -U -V -B --settings ${SCRIPT_PATH}/maven/settings.xml clean deploy -Pprepare-deploy -Pdeploy -f "${SCRIPT_PATH}/../pom.xml"
